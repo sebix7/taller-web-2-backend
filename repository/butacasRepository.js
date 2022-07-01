8007bbf1edc75913ceb094e1080b8dbce74c7618
julieta96 <julietaextra14@gmail.com>
2022-07-01 05:31:13 -0300

    update repositorio butacas

--- repository/butacasRepository.js
+++ repository/butacasRepository.js
@@ -6,8 +6,32 @@ const getButacas = () => {
         return butacas;
 };
 
+const updateButaca = async (data) => {
+	const request = await Model.updateOne({ columna: data.columna }, data);
+	return request;
+};
+
+const getButaca = async (req, res) => {
+	//const response = await Model.findOne({ columna }, { _id: 0});
+	//return response;
+
+    try{
+	const butaca = await Model.findOne({columna: req.params.columna }, { _id: 0});
+        if(!butaca){
+            res.status(404).json({msg:'no existe la butaca'})
+        }
+
+        res.json(butaca)
+    }catch(error){
+
+        console.log(error)
+        res.status(500).send('algun error')
+
+    }
+};
+
 module.exports = {
-	//getReserva
-	getButacas
-	//updateReserva
+	getButacas,
+	updateButaca,
+    getButaca
 };
