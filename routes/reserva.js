0d41db2806dbc8117a711c61ac695102a82edae5
julieta96 <julietaextra14@gmail.com>
2022-07-01 04:51:59 -0300

    cambio de estado butacas

--- routes/reserva.js
+++ routes/reserva.js
@@ -2,28 +2,15 @@ const { Router } = require("express");
 const Butacas = require("../models/Butacas");
 const router = Router();
 const reservaController= require("../controllers/reserva");
+const butacataController= require("../controllers/butacas");
 const butacaRepositorio = require("../repository/butacasRepository");
-//const reservaRepositorio = require("../repository/reservaRepository");
 
 router.get("/", async(req, res) => {
 	const butacas = await butacaRepositorio.getButacas();
 	return res.status(200).json({ butacas: butacas });
   });
   
-/*router.get("/:id", async (req, res) => {
-	let id = parseInt(req.params.id);
-	const reserva = await getReserva(id);
-	if (!reserva) {
-		return res
-			.status(404)
-			.json({ error: true, mensaje: "Reserva no encontrada" + id });
-	}
-	return res.json(reserva);
-});*/
-
-//router.get("/:id",getReserva)
 router.post("/",reservaController.nuevaReserva);
-
-//router.put("/:id",updateReserva)
-
+router.put("/:columna",butacataController.editarButaca);
+router.get("/:columna",butacaRepositorio.getButaca);
 module.exports = router;
